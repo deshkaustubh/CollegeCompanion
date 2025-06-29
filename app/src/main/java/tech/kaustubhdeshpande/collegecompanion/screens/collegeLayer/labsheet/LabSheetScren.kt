60efe9b5b2a13d3c7145c65280e1f170266c5b5f
Kaustubh Deshpande <162901396+deshkaustubh@users.noreply.github.com>
2025-06-29 21:56:06 +0530

    Refactor imports in LabSheetScreen.kt

--- app/src/main/java/tech/kaustubhdeshpande/collegecompanion/screens/collegeLayer/labsheet/LabSheetScren.kt
+++ app/src/main/java/tech/kaustubhdeshpande/collegecompanion/screens/collegeLayer/labsheet/LabSheetScren.kt
@@ -3,13 +3,10 @@ package tech.kaustubhdeshpande.collegecompanion.screens.collegeLayer.labsheet
 import android.annotation.SuppressLint
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContent
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Tune
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -24,7 +21,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
