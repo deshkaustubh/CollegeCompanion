b54b4518b5589723d68d39f2055718276958eb10
Kaustubh Deshpande <162901396+deshkaustubh@users.noreply.github.com>
2025-08-14 00:46:59 +0530

    Bump versionCode to 3 for release updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "tech.kaustubhdeshpande.collegecompanion"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
